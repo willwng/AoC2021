8d3c3d33a2ab74f8544d374126de2a09ba272566
William Wang <wyw6@cornell.edu>
2021-12-02 01:00:26 -0500

    1p2

--- 1/sol2.f
+++ 1/sol2.f
@@ -1,14 +1,18 @@
       PROGRAM SOLVE
-        INTEGER ANS=0
-        INTEGER PREV=HUGE(INTEGER)
-        INTEGER CURR=0
+        INTEGER ANS, PREV,PREV2,PREV3,CURR
+        PREV=HUGE(INTEGER)
+        PREV2=HUGE(INTEGER)
+        PREV3=HUGE(INTEGER)
+        CURR=0
+        ANS=0
         OPEN(1, FILE='input.txt', STATUS='old')
-
-
         DO I=1,2000
           READ(1,*) CURR
           IF(CURR.GT.PREV)ANS=ANS+1
-          PREV=CURR
+          PREV=PREV2
+          PREV2=PREV3
+          PREV3=CURR
+
         END DO
         PRINT *, "ANSWER IS ", ANS
       END
